086ff7294656a919824f8ca47a01aefd3dcd2892
Cristian Villavicencio <35142311+CristianRolendioVillavicencio@users.noreply.github.com>
2024-05-31 14:47:09 -0400

    mail

--- form/process-question.php
+++ form/process-question.php
@@ -1,6 +1,6 @@
 <?php
 
-$to = "";  // Your email here
+$to = "cristian_villavicencio@live.com";  // Your email here
 $from = $_REQUEST['email'];
 $name = $_REQUEST['name'];
 //$phone = $_REQUEST['phone'];
