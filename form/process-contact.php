c1a32922167649ee0597ae0230e2c3cf6dc631c5
Cristian Villavicencio <35142311+CristianRolendioVillavicencio@users.noreply.github.com>
2024-05-31 15:38:31 -0400

    cambios de form

--- form/process-contact.php
+++ form/process-contact.php
@@ -1,23 +1,30 @@
 <?php
 
 $to = "cristian_villavicencio@live.com";  // Your email here
-$from = $_REQUEST['email'];
-$name = $_REQUEST['name'];
-//$phone = $_REQUEST['phone'];
-$message = $_REQUEST['message'];
+$from = isset($_REQUEST['email']) ? $_REQUEST['email'] : '';
+$name = isset($_REQUEST['name']) ? $_REQUEST['name'] : '';
+//$phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : '';
+$message = isset($_REQUEST['message']) ? $_REQUEST['message'] : '';
 $headers = "From: $from";
-$subject = "Contact Form from Cleaning Service";
+$subject = "Question Form from Cleaning Service";
 
 $fields = array();
-$fields{"name"} = "First name";
-$fields{"email"} = "Email";
-//$fields{"phone"} = "Phone";
-$fields{"message"} = "Message";
+$fields["name"] = "First name";
+$fields["email"] = "Email";
+//$fields["phone"] = "Phone";
+$fields["message"] = "Message";
 
 $body = "Here is what was sent:\n\n";
 foreach($fields as $a => $b){   
-	$body .= sprintf("%20s:%s\r\n",$b,$_REQUEST[$a]);
+    $body .= sprintf("%20s: %s\r\n", $b, ${$a});
 }
+
 $send = mail($to, $subject, $body, $headers);
+
+if($send) {
+    echo "Mail sent successfully!";
+} else {
+    echo "Mail sending failed!";
+}
 
 ?>
